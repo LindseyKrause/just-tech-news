fd80c98098d5c69d8672c00419a09bb67f54c2a6
Lindsey Krause <79954805+LindseyKrause@users.noreply.github.com>
2021-06-07 22:23:21 -0700

    added login/out functionality

--- server.js
+++ server.js
@@ -9,17 +9,32 @@ const hbs = exphbs.create({});
 const app = express()
 const PORT = process.env.PORT || 3001;
 
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
+//express-session-----------------------
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
+//Handlebars-----------------------
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
-
+//Express-----------------------
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
-// turn on routes
+// turn on routes-----------------------
 app.use(routes);
 
-// turn on connection to db and server
+// turn on connection to db and server-----------------------
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });

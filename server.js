745a3666755cd1beae585eaa7e54d81d8d677c7a
Lindsey Krause <79954805+LindseyKrause@users.noreply.github.com>
2021-06-07 21:17:23 -0700

    refactor

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const PORT = process.env.PORT || 3001;
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));

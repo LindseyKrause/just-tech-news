6fea676ebc5c1b9668b9d1f5517ce6adda21b2b1
Lindsey Krause <79954805+LindseyKrause@users.noreply.github.com>
2021-06-07 20:58:27 -0700

    view functionality-initial

--- config/connection.js
+++ config/connection.js
@@ -1,4 +1,4 @@
-onst Sequelize = require('sequelize');
+const Sequelize = require('sequelize');
 
 require('dotenv').config();
 
@@ -7,7 +7,7 @@ let sequelize;
 if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
-  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+  sequelize = new Sequelize(process.env.just_tech_news_db, process.env.root, process.env.PASSWORD, {
     host: 'localhost',
     dialect: 'mysql',
     port: 3306
